b83573b5be4972036472d783905af9dc860af586
benjyair <280995163@qq.com>
2021-02-23 14:51:40 +0800

    Update Main.py

--- src/Main.py
+++ src/Main.py
@@ -37,7 +37,6 @@ if __name__ == '__main__':
     scheduler = BlockingScheduler()
     scheduler.add_job(job, 'cron', day_of_week='1-5', hour=9, minute=0, timezone='Asia/Shanghai')
     scheduler.add_job(job, 'cron', day_of_week='1-5', hour=23, minute=30, timezone='Asia/Shanghai')
-    scheduler.add_job(job, 'cron', day_of_week='1-5', hour=14, minute=50, timezone='Asia/Shanghai')
     scheduler.start()
     print(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ": scheduler start")
 
